42bc5f8df91425b2ea0a876b94318c6eeb99a9b9
minjongbaek <minjongbaek@gamil.com>
2019-07-23 10:21:13 +0900

    2019-07-23
    --

--- src/users/exception/InfoNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package users.exception;
-
-public class InfoNotFoundException extends Exception {
-	public InfoNotFoundException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-}
