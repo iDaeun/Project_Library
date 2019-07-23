af54209160df345e8461b650a6990244459922bd
minjongbaek <minjongbaek@gamil.com>
2019-07-23 10:30:05 +0900

    --

--- src/users/exception/InvalidUserPasswordException.java
+++
@@ -1,8 +0,0 @@
-package users.exception;
-
-public class InvalidUserPasswordException extends Exception {
-	public InvalidUserPasswordException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-}
--- src/users/exception/RentException.java
+++
@@ -1,9 +0,0 @@
-package users.exception;
-
-
-public class RentException extends Exception {
-	public RentException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-}
